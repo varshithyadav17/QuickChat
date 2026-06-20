8dc3eba458206c7f831f1036f8bdde5e61553157
Varshith Yadav <varshithyadav541@gmail.com>
2026-06-21 00:43:15 +0530

    Fix Linux case-sensitive imports

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,5 @@
-import Message from "../models/message.js";
-import User from "../models/User.js";
+import Message from "../models/Message.js"
+import User from "../models/User.js"
 import cloudinary from "../lib/cloudinary.js"
 import { io, userSocketMap } from "../server.js"
 
